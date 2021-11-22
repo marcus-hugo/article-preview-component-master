fb37690c8034e8e769ea893fc544ca867fa35fac
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2021-11-21 21:17:10 -0600

    updates

--- app/app.js
+++ app/app.js
@@ -1,1 +1,16 @@
 // alert('hello,marcus!')
+const popup = document.querySelector('.s-links-a');
+const btn = document.querySelector('.s-links__share');
+const icons = document.querySelectorAll('.icon');
+const path = document.querySelector('path');
+
+btn.addEventListener('click', function() {
+   btn.classList.toggle('spin');
+   popup.classList.toggle('active');
+
+   for (let icon of icons) {
+    //    icon.classList.remove('icon');
+        icon.classList.toggle('icon-grow');
+    }
+    
+})
