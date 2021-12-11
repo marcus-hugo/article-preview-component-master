11b20ebd6cf01fecda7fbd9bbe6b7205adfcceab
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2021-12-11 14:53:43 -0600

    removed uneeded code

--- app/app.js
+++ app/app.js
@@ -1,14 +1,12 @@
-// alert('hello,marcus!')
+
 const popup = document.querySelector('.s-links-a');
 const btn = document.querySelector('.s-links__share');
-const icons = document.querySelectorAll('.icon');
-const path = document.querySelector('path');
 
 btn.addEventListener('click', function() {
    
    btn.classList.remove('animate');
    void btn.offsetWidth;
    btn.classList.add('animate');
-   
+
    popup.classList.toggle('active');
 })
